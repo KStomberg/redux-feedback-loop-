a5f1a59893bb01a5e5f43b49539c9187fedd168d
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-09-27 09:03:54 -0500

    form completion function assembled for SubmitForm

--- src/components/SubmitForm/SubmitForm.js
+++ src/components/SubmitForm/SubmitForm.js
@@ -4,6 +4,13 @@ import {withRouter} from 'react-router-dom';
 
 class SubmitForm extends Component {
 
+    onCompletion = () => {
+        alert('Form submitted!');
+        this.props.sendFeedback();
+        console.log('Form completed, sending you home => /');
+        this.props.history.push('/');
+    }
+
     render() {
         return(
             <h1>TEST</h1>
