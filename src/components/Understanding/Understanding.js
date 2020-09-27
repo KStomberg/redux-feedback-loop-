8cbd7009127535e6dce899c8e7b7ab878e72ba36
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-09-26 20:32:25 -0500

    set client side for sending in understanding

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -4,10 +4,29 @@ import {withRouter} from 'react-router-dom';
 
 class Understanding extends Component {
 
+    updateUnderstanding = (event) => {
+        this.props.dispatch({
+            type: 'SET_Understanding',
+            payload: event.target.value
+        });
+        console.log('event.target.value', event.target.value);
+    }
+
+    onNext = () => {
+        console.log('Moving to next page => Supported');
+        this.props.history.push('/supported')
+    }
+
     render() {
         return(
             <div>
-                <h1>Test</h1>
+                <h1>On a scale of 1 to 6, how would you rate your understanding of todays topics?</h1>
+                <input required
+                    type="number"
+                    className="input"
+                    onChange={this.updateUnderstanding}
+                />
+                <button onClick={this.onNext} > Next </button>
             </div>
         )
     }
